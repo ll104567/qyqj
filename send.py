45293ec4d09dc780016a24f180ffdb9f27f20cb8
Dsz <ll104567i@163.com>
2019-11-27 12:33:43 +0800

    modify passeord

--- send.py
+++ send.py
@@ -8,7 +8,7 @@ from email.mime.multipart import MIMEMultipart
 
 HOST = 'smtp.163.com'  #smtp服务器地址
 SUBJECT = 'title'    # title 
-FROM = 'll104567i@163.com' # 你发送邮箱
+FROM = 'xxx@163.com' # 你发送邮箱
 
 TO = '1045670921@qq.com'	#往哪发
 TEXT = 'This is a test 2'
@@ -25,7 +25,7 @@ message['Subject'] = SUBJECT
 email_client = smtplib.SMTP_SSL()
 email_client.connect(HOST, '465')
 print('loading...')
-result = email_client.login(FROM, 'waawdr135')
+result = email_client.login(FROM, 'password') #这写密码
 print('Notication:', result)
 email_client.sendmail(from_addr=FROM, to_addrs=TO.split(','), msg=message.as_string())
 email_client.close()
